1f0730389dabe137b2d565fd4b7a33720c2ec8fd
fairuz1 <akbarazariafairuz@gmail.com>
2022-10-05 22:07:21 +0700

    data pertemuan_7

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -23,10 +23,25 @@
             <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
-            <a href="{{ asset('/Views')}}" class="nav-link">Home</a>
+            <a href="{{ asset('/Views')}}" class="nav-link active">Home</a>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
-            <a href="{{('/posts')}}" class="nav-link">Impressions</a>
+            <a href="{{('/posts')}}" class="nav-link active">Impressions</a>
+        </li>
+        <li class="nav-item dropdown">
+            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                {{ Auth::user()->name }}
+            </a>
+
+            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
+            </div>
         </li>
     </ul>
   
